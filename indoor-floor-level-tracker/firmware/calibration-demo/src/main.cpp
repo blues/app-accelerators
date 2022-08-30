0bd3782bcf4a868b5d88d4e41a049d1f9b39267a
Brandon Satrom <bsatrom+gh@gmail.com>
2022-08-30 17:33:01 -0500

    feat: add support for ATTN and envvars for settings

--- indoor-floor-level-tracker/firmware/calibration-demo/src/main.cpp
+++ indoor-floor-level-tracker/firmware/calibration-demo/src/main.cpp
@@ -9,25 +9,33 @@
 
 #define serialDebugOut Serial
 #define PRODUCT_UID "com.blues.nf1"
-Notecard notecard;
-
-#define BUTTON_A  9
-#define BUTTON_B  6
-#define BUTTON_C  5
+#define ATTN_INPUT_PIN 5
+#define DEBUG true
 
 unsigned long startMillis;
 unsigned long currentMillis;
 const unsigned long period = 1000 * 300;
 
-uint8_t lastFloor = 0;
-float floorHeight = 4.2672; // Height in meters (14 ft)
-float seaLevelPressure = 986.60; // Starting Sea Level Pressure in HPa
-float firstFloorAltitude = 0;
-float firstFloorPressure = seaLevelPressure;
+struct environmentVariables {
+  bool live;
+  double floorHeight;
+  int baselineFloor;
+  float baselineFloorPressure;
+  int noMovementThreshold;
+};
 
-bool setFirstFloor = false;
+bool setBaselineFloor = false;
+environmentVariables envVars;
+
+// Set to true whenever ATTN interrupt occurs
+static bool attnInterruptOccurred;
+
+Notecard notecard;
 
 // Forward declarations
+void attnISR(void);
+void attnArm();
+void fetchEnvironmentVariables(environmentVariables);
 void captureAndSendReadings(void);
 
 Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
@@ -56,27 +64,23 @@ void setup() {
   serialDebugOut.println("BMP581 connected...");
 
   /* Highest accuracy for stationary devices */
-  bmp.setTemperatureOversampling(BMP5_OVERSAMPLING_8X);
-  bmp.setPressureOversampling(BMP5_OVERSAMPLING_128X);
-  bmp.setIIRFilterCoeff(BMP5_IIR_FILTER_COEFF_127);
-  bmp.setOutputDataRate(BMP5_ODR_10_HZ);
+  // bmp.setTemperatureOversampling(BMP5_OVERSAMPLING_8X);
+  // bmp.setPressureOversampling(BMP5_OVERSAMPLING_128X);
+  // bmp.setIIRFilterCoeff(BMP5_IIR_FILTER_COEFF_127);
+  // bmp.setOutputDataRate(BMP5_ODR_10_HZ);
 
-  /* Suggestions from Bosch for movement */
-  // bmp.setTemperatureOversampling(BMP5_OVERSAMPLING_2X);
-  // bmp.setPressureOversampling(BMP5_OVERSAMPLING_16X);
-  // bmp.setIIRFilterCoeff(BMP5_IIR_FILTER_COEFF_3);
-  // bmp.setOutputDataRate(BMP5_ODR_50_HZ);
+  /* Suggestions from Bosch for devices in motion */
+  bmp.setTemperatureOversampling(BMP5_OVERSAMPLING_2X);
+  bmp.setPressureOversampling(BMP5_OVERSAMPLING_16X);
+  bmp.setIIRFilterCoeff(BMP5_IIR_FILTER_COEFF_3);
+  bmp.setOutputDataRate(BMP5_ODR_50_HZ);
 
   // Throw the first one away
   bmp.performReading();
 
-  // Clear the buffer.
+  // Clear the display buffer.
   display.clearDisplay();
   display.display();
-
-  pinMode(BUTTON_A, INPUT_PULLUP);
-  pinMode(BUTTON_B, INPUT_PULLUP);
-  pinMode(BUTTON_C, INPUT_PULLUP);
 
   // text display tests
   display.setRotation(2);
@@ -99,6 +103,26 @@ void setup() {
   JAddBoolToObject(req, "on", true);
   notecard.sendRequest(req);
 
+  // Check Environment Variables
+  fetchEnvironmentVariables(envVars);
+
+  // Disarm ATTN To clear any previous state before rearming
+  req = notecard.newRequest("card.attn");
+  JAddStringToObject(req, "mode", "disarm,-env");
+  notecard.sendRequest(req);
+
+  // Configure ATTN to wait for a specific list of files
+  req = notecard.newRequest("card.attn");
+  JAddStringToObject(req, "mode", "arm,env");
+  notecard.sendRequest(req);
+
+  // Attach an interrupt pin
+  pinMode(ATTN_INPUT_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(ATTN_INPUT_PIN), attnISR, RISING);
+
+  // Arm the interrupt, so that we are notified whenever ATTN rises
+  attnArm();
+
   delay(2000);
 
   captureAndSendReadings();
@@ -106,47 +130,104 @@ void setup() {
 }
 
 void loop() {
-  currentMillis = millis();
 
-  if (currentMillis - startMillis >= period) {
-    captureAndSendReadings();
+  if (attnInterruptOccurred) {
+    fetchEnvironmentVariables(envVars);
 
-    startMillis = currentMillis;
+    attnArm();
   }
+
+  if (!envVars.live) {
+    currentMillis = millis();
+
+    if (currentMillis - startMillis >= period) {
+      captureAndSendReadings();
+
+      startMillis = currentMillis;
+    }
+  } else {
+    // Behave differently if we are live
+  }
+}
+
+void fetchEnvironmentVariables(environmentVariables vars) {
+  J *req = NoteNewRequest("env.get");
+
+  J *names = JAddArrayToObject(req, "names");
+  JAddItemToArray(names, JCreateString("active"));
+  JAddItemToArray(names, JCreateString("baseline_floor"));
+  JAddItemToArray(names, JCreateString("floor_height"));
+  JAddItemToArray(names, JCreateString("no_movement_threshold"));
+
+  J *rsp = NoteRequestResponse(req);
+  if (rsp != NULL) {
+      if (notecard.responseError(rsp)) {
+          notecard.deleteResponse(rsp);
+          return;
+      }
+
+      // Get the note's body
+      J *body = JGetObject(rsp, "body");
+      if (body != NULL) {
+          vars.baselineFloor = atoi(JGetString(body, "baseline_floor"));
+          vars.floorHeight = atof(JGetString(body, "floor_height"));
+          vars.noMovementThreshold = atoi(JGetString(body, "no_movement_threshold"));
+          char *liveStr = JGetString(body, "live");
+
+          if (liveStr == "true" || liveStr == "1") {
+            vars.live = true;
+          } else {
+            vars.live = false;
+          }
+
+          serialDebugOut.printf("\nBaseline Floor: %d\n", vars.baselineFloor);
+          serialDebugOut.print("Floor Height: ");
+          serialDebugOut.print(vars.floorHeight);
+          serialDebugOut.printf("\nMovement Threshold: %d\n", vars.noMovementThreshold);
+          serialDebugOut.printf("Live: %s\n\n", vars.live ? "true" : "false");
+
+          setBaselineFloor = true;
+          envVars = vars;
+      }
+
+  }
+  notecard.deleteResponse(rsp);
 }
 
 void captureAndSendReadings() {
   if (! bmp.performReading()) {
-      Serial.println("Failed to perform reading :(");
+      serialDebugOut.println("Failed to perform reading from pressure sensor.");
       return;
     }
 
     float temp = bmp.temperature;
     float pressure = bmp.pressure / 100.0;
 
-    if (setFirstFloor) {
-      firstFloorPressure = pressure;
+    if (setBaselineFloor) {
+      envVars.baselineFloorPressure = pressure;
+
+      setBaselineFloor = false;
     }
 
-    float altitude = bmp.readAltitude(firstFloorPressure);
-    int8_t floor = altitude / floorHeight + 1;
+    float altitude = bmp.readAltitude(envVars.baselineFloorPressure);
+    int8_t floor = (altitude / envVars.floorHeight) + 1;
 
-    Serial.print("Temperature = ");
-    Serial.print(temp);
-    Serial.println(" *C");
+    serialDebugOut.print("Temperature = ");
+    serialDebugOut.print(temp);
+    serialDebugOut.println(" *C");
 
-    Serial.print("Pressure = ");
-    Serial.print(pressure);
-    Serial.println(" hPa");
+    serialDebugOut.print("Pressure = ");
+    serialDebugOut.print(pressure);
+    serialDebugOut.println(" hPa");
 
-    Serial.print("Approx. Altitude = ");
-    Serial.print(altitude);
-    Serial.println(" m");
+    serialDebugOut.print("Approx. Altitude = ");
+    serialDebugOut.print(altitude);
+    serialDebugOut.println(" m");
 
-    Serial.print("Floor = ");
-    Serial.println(floor);
+    serialDebugOut.print("Floor = ");
+    serialDebugOut.println(floor);
 
-    Serial.println();
+    serialDebugOut.println();
 
     display.clearDisplay();
     display.setCursor(0, 0);
@@ -168,4 +249,23 @@ void captureAndSendReadings() {
     JAddNumberToObject(body, "temp", temp);
     JAddItemToObject(req, "body", body);
     notecard.sendRequest(req);
+}
+
+void attnISR()
+{
+    attnInterruptOccurred = true;
+}
+
+// Re-arm the ATTN interrupt
+void attnArm()
+{
+    // Make sure that we pick up the next RISING edge of the interrupt
+    attnInterruptOccurred = false;
+
+    // Set the ATTN pin low and reset to the previous state of waiting for
+    // Env var updates
+    J *req = notecard.newRequest("card.attn");
+    JAddStringToObject(req, "mode", "reset");
+    notecard.sendRequest(req);
+
 }
