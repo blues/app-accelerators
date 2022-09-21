642354839e8071c0559c06d3b44770509b9639b1
Paige Niedringhaus <paigen11@gmail.com>
2022-09-21 14:07:37 +0000

    add simple functions to separate out updating individual fleet vars and calling the api to do the updating itself

--- indoor-floor-level-tracker/web-app/src/api-client/fleetVariables.ts
+++ indoor-floor-level-tracker/web-app/src/api-client/fleetVariables.ts
@@ -2,21 +2,25 @@
 import axios, { AxiosResponse } from "axios";
 import { services } from "../services/ServiceLocatorClient";
 
-export async function updateLiveTrackerStatus(newTrackerConfig: boolean) {
-  // todo ask team if it makes more sense to have one fuction to handle all fleet updates or multiple functions handling just one particular kind
-  // update the entire trackerconfig obj with whatever new fleet value needs to change
-  const updateLiveFleetTracking = {
-    live: newTrackerConfig,
-  };
-
+async function updateFleetEnvVar(fleetEnvVarToUpdate: object) {
   const endpoint = services()
     .getUrlManager()
-    .setFleetTrackerConfig(updateLiveFleetTracking);
+    .setFleetTrackerConfig(fleetEnvVarToUpdate);
 
   const response: AxiosResponse = await axios.post(
     endpoint,
-    updateLiveFleetTracking
+    fleetEnvVarToUpdate
   );
-  console.log("Response back from update tracker status! ", response);
-  return response.data;
+
+  return response.data as object;
+}
+
+export async function updateLiveTrackerStatus(liveTrackerStatus: boolean) {
+  const updateLiveFleetTrackingObj = {
+    live: liveTrackerStatus,
+  };
+
+  const response = await updateFleetEnvVar(updateLiveFleetTrackingObj);
+
+  return response;
 }
