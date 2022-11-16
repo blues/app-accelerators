65e8b7dd7af4ba22719d64c30ad7cc723f08dfce
Paige Niedringhaus <paigen11@gmail.com>
2022-11-16 19:50:05 +0000

    final pr comments addressed

--- nextjs-template/src/components/presentation/deviceEventInfo.ts
+++ nextjs-template/src/components/presentation/deviceEventInfo.ts
@@ -6,6 +6,34 @@ import {
   FleetEnvVars,
 } from "../../services/DomainModel";
 
+function filterEventsByDevice(device: Device, eventList: Event[]) {
+  // consider also filtering out certain event types (like alarms or notification events) as well to keep the data cleaner and more uniform
+  const filteredEvents = eventList
+    .filter((event) => event.deviceUID.deviceUID === device.id.deviceUID)
+    // sort events newest to oldest
+    .sort((a, b) => Number(new Date(b.when)) - Number(new Date(a.when)));
+
+  return filteredEvents;
+}
+
+function filterEnvVarsByDevice(device: Device, deviceEnvVars: DeviceEnvVars[]) {
+  const filteredDeviceEnvVars = deviceEnvVars.filter(
+    (deviceEnvVar) => deviceEnvVar.deviceID === device.id.deviceUID
+  );
+  return filteredDeviceEnvVars;
+}
+
+function filterEnvVarsByFleet(device: Device, fleetEnvVars: FleetEnvVars[]) {
+  /* operating under the assumption a device will only be assigned to one fleet at a time */
+  let filteredFleetEnvVars: FleetEnvVars[] = [];
+  if (fleetEnvVars.length) {
+    filteredFleetEnvVars = fleetEnvVars.filter(
+      (fleetEnvVar) => fleetEnvVar.fleetUID === device.fleetUIDs[0]
+    );
+  }
+  return filteredFleetEnvVars;
+}
+
 function assembleDeviceEventsObject(
   deviceID: string,
   deviceEnvVars: DeviceEnvVars[],
@@ -30,27 +58,15 @@ export function getNormalizedDeviceData(
   fleetEnvVars: FleetEnvVars[]
 ) {
   const deviceEventInfo = devices.map((device) => {
-    // consider also filtering out certain event types (like alarms or notification events) as well to keep the data cleaner and more uniform
-    const filterEventsByDevice = deviceEvents
-      .filter((event) => event.deviceUID.deviceUID === device.id.deviceUID)
-      // sort events newest to oldest
-      .sort((a, b) => Number(new Date(b.when)) - Number(new Date(a.when)));
+    // filter events by device
+    const filteredEventsByDevice = filterEventsByDevice(device, deviceEvents);
+    const updatedEventList = [...filteredEventsByDevice];
 
-    const updatedEventList = [...filterEventsByDevice];
+    // filter device env vars by device
+    const filteredDeviceEnvVars = filterEnvVarsByDevice(device, deviceEnvVars);
 
-    // filter correct device env vars
-    const filteredDeviceEnvVars = deviceEnvVars.filter(
-      (deviceEnvVar) => deviceEnvVar.deviceID === device.id.deviceUID
-    );
-
-    // filter correct fleet env vars
-    /* operating under the assumption a device will only be assigned to one fleet at a time */
-    let filteredFleetEnvVars: FleetEnvVars[] = [];
-    if (fleetEnvVars.length) {
-      filteredFleetEnvVars = fleetEnvVars.filter(
-        (fleetEnvVar) => fleetEnvVar.fleetUID === device.fleetUIDs[0]
-      );
-    }
+    // filter fleet env vars by fleet
+    const filteredFleetEnvVars = filterEnvVarsByFleet(device, fleetEnvVars);
 
     // reassemble each device with its events and env vars
     const updatedDeviceEventsObject = assembleDeviceEventsObject(
