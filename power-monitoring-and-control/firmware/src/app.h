a3eb7e8accdb9c01214342a2eecbdf6e3007d993
Matthew <mat.mcgowan+github@gmail.com>
2022-12-02 11:27:09 -0800

    Power Monitoring and Control (#42)
    * Arduino project from notepower repo. Added platformio.ini and changed PRODUCT_UID.
    
    * feat(power): adds apparent and reactive power and power factor to events.
    
    * feat(power): adds app name to notes
    
    * feat(power): gpio inputs from environment variables. adds `inputN` and `switchN` to the power event template, which are filled with `true`/`false` to reflect the GPIO state.
    
    * feat(power): implements line activity checks according tot the measured current/voltage and the state of the corresponding GPIO poin for a given power line, with startup and shutdown time to suppress spurious warnings during transient states.
    
    * feat(power): adds vibration monitoring and alerts
    
    * disable CDC USB Serial so that Serial maps to HardwareSerial (F_RX/F_TX) which is used to receive notifications over Serial from the notecard.
    
    * adds an event counter. Alarms are suppressed if the reason string has not changed since the previous event from a power monitor. This avoids sending many events when the cause of the alarm is still present.  Heartbeat events are still sent regardless of whether the reasons have changed or not. adds a test setting to always sync heartbeat events.
    
    * adds vibration aggregation
    
    * misc fixes after testing
    
    * Update power-monitoring-and-control/firmware/src/ARDUINO.txt
    
    Co-authored-by: Brandon Satrom <bsatrom+gh@gmail.com>
    
    * Update power-monitoring-and-control/firmware/src/ARDUINO.txt
    
    Co-authored-by: Brandon Satrom <bsatrom+gh@gmail.com>
    
    * formatting and comments from review
    
    Co-authored-by: Brandon Satrom <bsatrom+gh@gmail.com>

---
+++ power-monitoring-and-control/firmware/src/app.h
@@ -0,0 +1,48 @@
+// Copyright 2022 Blues Inc.  All rights reserved.
+// Use of this source code is governed by licenses granted by the
+// copyright holder including that found in the LICENSE file.
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <Notecard.h>
+#include "NoteRTOS.h"
+
+#pragma once
+
+#ifndef PRODUCT_UID
+#define PRODUCT_UID ""		// "com.my-company.my-name:my-project"
+#pragma message "PRODUCT_UID is not defined in this example. Please ensure your Notecard has a product identifier set before running this example or define it in code here. More details at https://bit.ly/product-uid"
+#endif
+
+// Define this if using USB serial, and comment it out if using the ST-Link V3
+// (See USING_SWAN.txt for more info.)
+// #define debug Serial
+
+// Define the debug output stream device, as well as a method enabling us
+// to determine whether or not the Serial device is available for app usage.
+#ifdef debug
+#define	serialIsAvailable() false
+#else
+#define	serialIsAvailable() true
+#ifdef APP_MAIN
+HardwareSerial debug(PG8, PG7);
+#else
+extern HardwareSerial debug;
+#endif
+#endif
+
+// Notecard definition
+#ifdef APP_MAIN
+Notecard notecard;
+#else
+extern Notecard notecard;
+#endif
+
+// app.cpp
+uint32_t appTasks(uint32_t **taskSchedMs, uint8_t **contextBase, uint32_t *contextSize);
+bool appSetup(void);
+uint32_t appLoop(void);
+bool taskSetup(void *mcp);
+uint32_t taskLoop(void *mcp);
+
+#define APP_NAME "nf8"
