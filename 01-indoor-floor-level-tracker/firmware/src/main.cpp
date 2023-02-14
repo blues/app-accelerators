1f97941f441b4cb3b49cd84a6d26d33dab19ad16
Paige Niedringhaus <paigen11@gmail.com>
2023-02-14 14:55:21 -0500

    revert: revert firmware back to original state

--- 01-indoor-floor-level-tracker/firmware/src/main.cpp
+++ 01-indoor-floor-level-tracker/firmware/src/main.cpp
@@ -32,7 +32,7 @@
 
 // This is the unique Product Identifier for your device
 #ifndef PRODUCT_UID
-#define PRODUCT_UID "com.blues.nf_prisma_test_project"
+#define PRODUCT_UID "" // "com.my-company.my-name:my-project"
 #pragma message "PRODUCT_UID is not defined in this example. Please ensure your Notecard has a product identifier set before running this example or define it in code here. More details at https://dev.blues.io/notehub/notehub-walkthrough/#finding-a-productuid"
 #endif
 
