325bd0c71db6e9903e7877e6ce060cd913cca62a
Paige Niedringhaus <paigen11@gmail.com>
2023-02-15 09:41:21 -0600

    refactor: update NF1 to use notehub-js library for data fetching from Notehub (#74)
    * fixing notification store for valve-monitor
    
    * first commit of nf10 web app
    
    * feat: nf10 code simplification (#62)
    
    * eliminating valve controls and replacing all valve device references with flow rate device instead
    
    * fixing css
    
    * updating some small css tweaks for nf9 as well
    
    * adding nf9 icon to folder
    
    * adding readmes for nf10
    
    * minor tweaks
    
    * adding back scripts to stand up db without docker help
    
    * wip commit testing notehub js openapi generated library
    
    * tweaks I made to make firmware setup for testing easier
    
    * testing out more of the notehub js openapi generator calls - so far, pretty good
    
    * refactor: begin swapping hand rolled notehub for notehub-js
    
    * deps: add notehub-js as dependency
    
    * refactor: swap out axios notehub calls for notehub-js
    
    * test: fix broken unit test
    
    * revert: revert firmware back to original state
    
    * revert: replace old img with new img
    
    * refactor: remove unused models
    
    * fix: typo
    
    * test: clean up test data

--- 01-indoor-floor-level-tracker/web-app/src/services/DataProvider.ts
+++ 01-indoor-floor-level-tracker/web-app/src/services/DataProvider.ts
@@ -1,10 +1,5 @@
 import { DeviceTracker, TrackerConfig } from "./AppModel";
 
-export interface BulkImport {
-  itemCount: number;
-  errorCount: number;
-}
-
 export interface DataProvider {
   getDeviceTrackerData(): Promise<DeviceTracker[]>;
   getTrackerConfig(): Promise<TrackerConfig>;
